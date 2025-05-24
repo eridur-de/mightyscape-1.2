93e6324c4a9cfaa4e9d613e654f8e0e69592c748
Jonathan Neuhauser <jonathan.neuhauser@outlook.com>
2025-05-24 11:50:14 +0200

    Add first working test case

--- extensions/fablabchemnitz/apply_transformations/tests/test_apply_transformations.py
+++ extensions/fablabchemnitz/apply_transformations/tests/test_apply_transformations.py
@@ -1,18 +1,19 @@
 # coding=utf-8
-from apply_transformations import ApplyTransformations
+from apply_transformations.apply_transformations import ApplyTransformations
 from inkex.tester import ComparisonMixin, TestCase
 from inkex.tester.filters import CompareNumericFuzzy, CompareWithPathSpace
 
 
 class ApplyTransformations(ComparisonMixin, TestCase):
-    effect_class = AddNodes
+    effect_class = ApplyTransformations
     comparisons = [
         tuple()
     ]
-    compare_file = "complextransforms.test.svg"
+    compare_file = "svg/complextransform.test.svg"
     compare_filters = [
         CompareWithPathSpace(),
         CompareNumericFuzzy(),
     ]
+    stderr_protect = False
 
 
