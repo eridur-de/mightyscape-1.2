a3f05fd501c01df190fa21f921231c9cd6123515
Mario Voigt <mario.voigt@stadtfabrikanten.org>
2025-06-01 13:44:02 +0200

    add sample line

--- extensions/fablabchemnitz/000_update-json.sh
+++ extensions/fablabchemnitz/000_update-json.sh
@@ -5,6 +5,7 @@ for folder in */ ; do
 	if [[ -f "${folder}/meta.json" ]]; then
 		sed -i 's/github.com\/vmario89/github.com\/eridur-de/g' ${folder}/meta.json
 		sed -i 's/mightyscape-1.X/mightyscape-1.2/g' ${folder}/meta.json
-		
-	fi
+		#add dependent_extensions to meta.json
+		#sed -i '6i     "dependent_extensions": null,' ${folder}/meta.json
+fi
 done
