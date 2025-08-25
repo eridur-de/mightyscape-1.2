b8ae76d984a6f1217aec0bd17a17dc36758fcd7d
Mario Voigt <mario.voigt@stadtfabrikanten.org>
2025-08-26 01:29:18 +0200

    remove obsolete stuff from insert_paper_template

--- extensions/fablabchemnitz/insert_paper_template/insert_paper_template.py
+++ extensions/fablabchemnitz/insert_paper_template/insert_paper_template.py
@@ -35,48 +35,51 @@ class InsertPaperTemplate(inkex.EffectExtension):
 		self.Group = etree.SubElement(self.svg.get_current_layer(), inkex.addNS('g','svg'), {} )
 
 		papertypes = {}
-		papertypes["A7"] = (74,  105, "#7f7f7f") # grey
-		papertypes["A6"] = (105, 148, "#00ffff") # cyan
-		papertypes["A5"] = (148, 210, "#ffeeaa") # yellow
-		papertypes["A4"] = (210, 297, "#ffccaa") # orange
-		papertypes["A3"] = (297, 420, "#afdde9") # blue
-		papertypes["A2"] = (420, 594, "#ccaaff") # purple
-		papertypes["A1"] = (594, 841, "#afe9c6") # green
-		papertypes["A0"] = (841, 1189, "#ffd5d5") # red
-		papertypes["B6"] = (125, 176, "#ffccaa") # orange
-		papertypes["B5"] = (176, 250, "#afdde9") # blue
-		papertypes["C6"] = (114, 162, "#ccaaff") # purple
-		papertypes["C5"] = (162, 228, "#afe9c6") # green
-		papertypes["DIN_LANG"] = (110, 220, "#ffd5d5") # red
-		papertypes["POKER"] = (63.5, 88, "#ffffff") # white
-		papertypes["BRIDGE"] = (56, 88, "#ffffff") # white
-		papertypes["MINI_US"] = (41, 63, "#ffffff") # white
-		papertypes["MINI_EU"] = (44, 68, "#ffffff") # white
-		papertypes["TAROT"] = (70, 120, "#ffffff") # white
-
+		papertypes["A7"]       = (74  , 105 , "#7f7f7f") # grey
+		papertypes["A6"]       = (105 , 148 , "#00ffff") # cyan
+		papertypes["A5"]       = (148 , 210 , "#ffeeaa") # yellow
+		papertypes["A4"]       = (210 , 297 , "#ffccaa") # orange
+		papertypes["A3"]       = (297 , 420 , "#afdde9") # blue
+		papertypes["A2"]       = (420 , 594 , "#ccaaff") # purple
+		papertypes["A1"]       = (594 , 841 , "#afe9c6") # green
+		papertypes["A0"]       = (841 , 1189, "#ffd5d5") # red
+		papertypes["B6"]       = (125 , 176 , "#ffccaa") # orange
+		papertypes["B5"]       = (176 , 250 , "#afdde9") # blue
+		papertypes["C6"]       = (114 , 162 , "#ccaaff") # purple
+		papertypes["C5"]       = (162 , 228 , "#afe9c6") # green
+		papertypes["DIN_LANG"] = (110 , 220 , "#ffd5d5") # red
+		papertypes["POKER"]    = (63.5, 88  , "#ffffff") # white
+		papertypes["BRIDGE"]   = (56  , 88  , "#ffffff") # white
+		papertypes["MINI_US"]  = (41  , 63  , "#ffffff") # white
+		papertypes["MINI_EU"]  = (44  , 68  , "#ffffff") # white
+		papertypes["TAROT"]    = (70  , 120 , "#ffffff") # white
 
 		if self.options.papertype in papertypes:
 			self.CreateTemplate(self.options.papertype, *(papertypes[self.options.papertype]))
 		else:
 			raise Exception("Paper type '{0}' is undefined".format(self.options.papertype))
 
-	def CreateTemplate(self, label, width, height, colour):
+	def CreateTemplate(self, label, width, height, color):
 		x = 0
 		y = 0
-		self._CreateRectangleInMillimetres(width, height, x, y, colour)
+		self._CreateRectangleInMillimetres(width, height, x, y, color)
 		if self.options.show_type is True:
 			self._CreateText(label, x + width/2 , y + height/2)
 
 	def _CreateText(self, labelText, x, y):
 		style = {'stroke': '#000000',
-			'stroke-width': self.MMtoUU(0.25),
+			'stroke-width': '0.25',
 			'fill' : '#000000',
 			'font-size' : "11px",
 			'text-align' : 'center',
 			'text-anchor' : 'middle'
 			}
 
-		attribs = {'style': str(inkex.Style(style)), 'x': self.MMtoUU(x), 'y': self.MMtoUU(y)}
+		attribs = {
+			'style': str(inkex.Style(style)), 
+			'x': "{}".format(x), 
+			'y': "{}".format(y)
+			}
 
 		text = etree.Element(inkex.addNS('text','svg'), attribs)
 		text.text = labelText
@@ -84,15 +87,19 @@ class InsertPaperTemplate(inkex.EffectExtension):
 		self.Group.append(text)
 
 	def _CreateRectangleInMillimetres(self, width, height, x, y, color):
-		style = {'stroke': '#000000', 'stroke-width': self.MMtoUU(0.25), 'fill' : color}
-		attribs = {'style': str(inkex.Style(style)), 'height': self.MMtoUU(height), 'width': self.MMtoUU(width), 'x': self.MMtoUU(x), 'y': self.MMtoUU(y)}
+		style = {
+			'stroke': '#000000', 
+			'stroke-width': '0.25', 
+			'fill' : color
+			}
+		attribs = {
+			'style': str(inkex.Style(style)), 
+			'height': "{}".format(height), 
+			'width': "{}".format(width), 
+			'x': "{}".format(x), 
+			'y': "{}".format(y)
+			}
 		etree.SubElement(self.Group, inkex.addNS('rect','svg'), attribs )
-
-	def MMtoUU(self, mmval):
-		if hasattr(self.svg, "unittouu"):
-			return str(self.svg.unittouu("{0}mm".format(mmval)))
-		else:
-			return str(mmval)
 
 if __name__ == '__main__':
 	InsertPaperTemplate().run()
