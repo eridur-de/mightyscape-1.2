0f0ff9baaf29a42671e16f839d2e3bc56af01325
Mario Voigt <mario.voigt@stadtfabrikanten.org>
2025-08-26 12:35:01 +0200

    add option to merge tspans

--- extensions/fablabchemnitz/text_replace/text_replace.py
+++ extensions/fablabchemnitz/text_replace/text_replace.py
@@ -20,15 +20,36 @@ class ReplaceText(inkex.EffectExtension):
     
     def add_arguments(self, pars):
         pars.add_argument("--replacewith", default='lorem ipsum', help="New content to insert")
+        pars.add_argument("--tspan_merge", default=True, type=inkex.Boolean, help="Merge tspan elements")
 
-    def parse(self, element):
+    def adjust(self, element):
         if element.tag == inkex.addNS('tspan','svg'):
             element.text = self.options.replacewith
+
+    def mergeTspans(self, element):
+        tspans = []
+        merged_text = ""
+        if element.tag == inkex.addNS('text','svg'):
+            children = element.getchildren()
+            if children is not None:
+                for child in children:
+                    if child.tag == inkex.addNS('tspan','svg'):
+                        tspans.append(child)
+        for tspan in tspans:
+            merged_text = "{} {}".format(merged_text, tspan.text)
+        if len (tspans) > 0:
+            tspans[0].text = merged_text
+            for k in range(1, len(tspans)):
+                tspans[k].getparent().remove(tspans[k])
+
+    def parse(self, element):
+        if self.options.tspan_merge is True:
+            self.mergeTspans(element)
+        self.adjust(element)
         children = element.getchildren()
         if children is not None:
             for child in children:
-                if element.tag == inkex.addNS('tspan','svg'):
-                    element.text = self.options.replacewith
+                self.adjust(element)
                 self.parse(child)
 
     def effect(self):
